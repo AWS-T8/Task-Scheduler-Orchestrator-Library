8c1a894cba20034876125787cb611de531397b3e
Sanskar Agarwal <sanskaragarwal05@gmail.com>
2021-03-16 17:40:34 +0530

    Change scripts

--- stop.sh
+++ stop.sh
@@ -1,8 +1,14 @@
-cd ./backend_apiContract
-forever stop server.js
-sleep 10
-cd ../scheduler
+echo 'Stoping API Server...\n'
+sudo docker rm $(sudo docker stop $(sudo docker ps -a -q -f ancestor=api-contract-img))
+echo '\nSleeping for 5 seconds...\n'
+sleep 5
+echo '\nStoping Scheduler...\n'
+# sudo docker rm $(sudo docker stop $(sudo docker ps -a -q -f ancestor=scheduler-img))
+cd /home/sanskar/AWS-T8/scheduler
 forever stop schedule.js
-sleep 10
 cd ..
+echo '\nSleeping for 5 seconds...\n'
+sleep 5
+echo '\nStoping Kafka & Mongo...\n'
 sudo docker-compose stop
+echo '\nAll Services Stopped!\n'
