446b86051daa361e9158540bd4567aefe2037c9a
Saksham Arora <saksham20189575@gmail.com>
2021-03-18 12:52:29 +0530

    Fixed Todos

--- backend_taskRunner/consumer.js
+++ backend_taskRunner/consumer.js
@@ -1,42 +1,42 @@
-var kafka = require('kafka-node');
-require('dotenv').config({ path: './.env' });
-
-const defaultTopicName = 'sch-task';
+var kafka = require("kafka-node");
+require("dotenv").config({ path: "./.env" });
+const connectDB = require("./configure/db.js");
+const defaultTopicName = "sch-task";
 const kafkaHost = process.env.KAFKA_URL;
-const { exec } = require('child_process');
+const { exec } = require("child_process");
 
-
+connectDB(); // Connecting DB in once in consumer
 const client = new kafka.KafkaClient({
-	kafkaHost: kafkaHost,
+  kafkaHost: kafkaHost,
 });
 
 const topics = [
-	{
-		topic: defaultTopicName,
-		partitions: 1,
-		replicationFactor: 1,
-	},
+  {
+    topic: defaultTopicName,
+    partitions: 1,
+    replicationFactor: 1,
+  },
 ];
 
 client.createTopics(topics, (err, result) => {
-	// console.log()
+  // console.log()
 });
 
 const consumer = new kafka.Consumer(client, [{ topic: defaultTopicName }], {
-	groupId: 'sch-tsk-cons-grp',
+  groupId: "sch-tsk-cons-grp",
 });
 
-consumer.on('message', function (message) {
-	command = `node ./taskRunner.js ${message.value}`;
-	exec(command, (error, stdout, stderr) => {
-		if (error) {
-			console.error(`exec error: ${error}`);
-			return;
-		}
-		// console.log(stderr);
-	});
+consumer.on("message", function (message) {
+  command = `node ./taskRunner.js ${message.value}`;
+  exec(command, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`exec error: ${error}`);
+      return;
+    }
+    // console.log(stderr);
+  });
 });
 
-consumer.on('error', function (error) {
-	console.log(error);
+consumer.on("error", function (error) {
+  console.log(error);
 });
--- backend_taskRunner/taskRunner.js
+++ backend_taskRunner/taskRunner.js
@@ -1,108 +1,100 @@
-const axios = require('axios');
-require('dotenv').config({
-	path: './.env',
+const axios = require("axios");
+require("dotenv").config({
+  path: "./.env",
 });
-const mongoose = require('mongoose');
-const taskDB = require('./models/taskDB');
-const ObjectID = require('mongodb').ObjectID;
-const fs = require('fs');
 
-mongoose.connect(process.env.DATABASE_URL, {
-	useCreateIndex: true,
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-});
-const db = mongoose.connection;
-db.on('error', (err) => console.error(err));
-db.once('open', () => {
-	console.log('Connected to Database');
-	const args = process.argv.slice(2);
-	if (args.length === 0) {
-		db.close();
-		process.exit(0);
-	}
-	const id = args[0];
+const taskDB = require("./models/taskDB");
+const ObjectID = require("mongodb").ObjectID;
+const fs = require("fs");
 
-	execTask(id);
-});
+/**
+ * @GlobalDB needs to verified
+ */
+const args = process.argv.slice(2);
+if (args.length === 0) {
+  db.close();
+  process.exit(0);
+}
+const id = args[0];
+execTask(id);
 
 const writeLog = async (taskLog) => {
-	let data = await JSON.stringify(taskLog, null, 2);
-	fs.writeFile(
-		`${process.env.OUTPUT_PATH}/${taskLog.id}-log.json`,
-		data,
-		(err) => {
-			if (err) {
-				throw err;
-			}
-			console.log('Data written to file');
-			db.close();
-			process.exit(0);
-		}
-	);
+  let data = await JSON.stringify(taskLog, null, 2);
+  fs.writeFile(
+    `${process.env.OUTPUT_PATH}/${taskLog.id}-log.json`,
+    data,
+    (err) => {
+      if (err) {
+        throw err;
+      }
+      console.log("Data written to file");
+      db.close();
+      process.exit(0);
+    }
+  );
 };
 
 const execTask = async (id) => {
-	if (!ObjectID.isValid(id)) {
-		db.close();
-		process.exit(0);
-	}
-	const currentTask = await taskDB.findById(id);
-	if (currentTask.status != 'scheduled') {
-		db.close();
-		process.exit(0);
-	}
-	currentTask.status = 'running';
-	//   let schTime = currentTask.execTime.toString().replace(/[{()}]/g, '');
-	let schTime = currentTask.execTime.toLocaleString('en-US', {
-		timeZone: 'Asia/Kolkata',
-	});
-	let execTime = new Date().toLocaleString('en-US', {
-		timeZone: 'Asia/Kolkata',
-	});
+  if (!ObjectID.isValid(id)) {
+    db.close();
+    process.exit(0);
+  }
+  const currentTask = await taskDB.findById(id);
+  if (currentTask.status != "scheduled") {
+    db.close();
+    process.exit(0);
+  }
+  currentTask.status = "running";
+  //   let schTime = currentTask.execTime.toString().replace(/[{()}]/g, '');
+  let schTime = currentTask.execTime.toLocaleString("en-US", {
+    timeZone: "Asia/Kolkata",
+  });
+  let execTime = new Date().toLocaleString("en-US", {
+    timeZone: "Asia/Kolkata",
+  });
 
-	currentTask.save().then((task) => {
-		axios
-			.get(task.url)
-			.then((result) => {
-				if (result.status === 200) {
-					task.status = 'completed';
-				} else {
-					task.status = 'failed';
-				}
-				task.save().then((res) => {
-					const completeTime = new Date().toLocaleString('en-US', {
-						timeZone: 'Asia/Kolkata',
-					});
-					const output = `schTime: ${schTime} || execTime: ${execTime} || completeTime: ${completeTime}\n`;
-					console.log(output);
-					let taskLog = {
-						id: res._id,
-						scheduledTime: schTime,
-						executionTime: execTime,
-						completetionTime: completeTime,
-						status: res.status,
-					};
-					writeLog(taskLog);
-				});
-			})
-			.catch((err) => {
-				task.status = 'failed';
-				const completeTime = new Date().toLocaleString('en-US', {
-					timeZone: 'Asia/Kolkata',
-				});
-				task.save().then((res) => {
-					const output = `schTime: ${schTime} || execTime: ${execTime} || completeTime: ${completeTime}\n`;
-					console.log(output);
-					let taskLog = {
-						id: res._id,
-						scheduledTime: schTime,
-						executionTime: execTime,
-						completetionTime: completeTime,
-						status: res.status,
-					};
-					writeLog(taskLog);
-				});
-			});
-	});
+  currentTask.save().then((task) => {
+    axios
+      .get(task.url)
+      .then((result) => {
+        if (result.status === 200) {
+          task.status = "completed";
+        } else {
+          task.status = "failed";
+        }
+        task.save().then((res) => {
+          const completeTime = new Date().toLocaleString("en-US", {
+            timeZone: "Asia/Kolkata",
+          });
+          const output = `schTime: ${schTime} || execTime: ${execTime} || completeTime: ${completeTime}\n`;
+          console.log(output);
+          let taskLog = {
+            id: res._id,
+            scheduledTime: schTime,
+            executionTime: execTime,
+            completetionTime: completeTime,
+            status: res.status,
+          };
+          writeLog(taskLog);
+        });
+      })
+      .catch((err) => {
+        task.status = "failed";
+        const completeTime = new Date().toLocaleString("en-US", {
+          timeZone: "Asia/Kolkata",
+        });
+        task.save().then((res) => {
+          const output = `schTime: ${schTime} || execTime: ${execTime} || completeTime: ${completeTime}\n`;
+          console.log(output);
+          let taskLog = {
+            id: res._id,
+            scheduledTime: schTime,
+            executionTime: execTime,
+            completetionTime: completeTime,
+            status: res.status,
+          };
+          writeLog(taskLog);
+        });
+      });
+  });
 };
