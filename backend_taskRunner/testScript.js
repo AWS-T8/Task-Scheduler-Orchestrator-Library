4d9453aff25275c051baa1a34e047bda212b7d5a
Sanskar Agarwal <sanskaragarwal05@gmail.com>
2021-03-19 09:34:28 +0530

    Improve test script

--- backend_taskRunner/testScript.js
+++ backend_taskRunner/testScript.js
@@ -3,6 +3,7 @@ var fs = require('fs');
 var sum_delay = 0;
 var sum_time_to_complete = 0;
 var count = 0;
+var max_delay = 0;
 
 // Get the contents of the directory and loop over it.
 fs.readdir('./logs', function (err, list) {
@@ -19,16 +20,18 @@ fs.readdir('./logs', function (err, list) {
 		var time_to_complete = (completetionTime - executionTime) / 1000;
 		// console.log(`${parsedData.id} ${delay} ${time_to_complete}`);
 		sum_delay += delay;
+		max_delay = Math.max(max_delay, delay);
 		sum_time_to_complete += time_to_complete;
 	}
 	console.log(`The number of files -> ${count}`);
 	if (count > 0) {
 		var avg_delay = sum_delay / count;
 		var avg_time_to_complete = sum_time_to_complete / count;
+		console.log(`Maximum Delay -> ${max_delay}s`);
 		console.log(
 			`AVG -> Time delay : ${avg_delay.toFixed(
 				3
-			)} Execution Time : ${avg_time_to_complete.toFixed(3)}`
+			)}s Execution Time : ${avg_time_to_complete.toFixed(3)}s`
 		);
 	}
 });
